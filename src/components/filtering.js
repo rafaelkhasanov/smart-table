3bc321713f65c71d12c96d4d7aa2d8ab375d5e16
rnkhasanov <RNKhasanov@kzn.beeline.ru>
2026-02-25 14:18:51 +0300

    add filtering

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,14 +1,30 @@
-import {createComparison, defaultRules} from "../lib/compare.js";
+import { createComparison, defaultRules } from "../lib/compare.js";
 
 // @todo: #4.3 — настроить компаратор
+const compare = createComparison(defaultRules);
 
 export function initFiltering(elements, indexes) {
-    // @todo: #4.1 — заполнить выпадающие списки опциями
+  // @todo: #4.1 — заполнить выпадающие списки опциями
+  Object.keys(indexes).forEach((elementName) => {
+    elements[elementName].append(
+      ...Object.values(indexes[elementName]).map((name) => {
+        return new Option(name, name);
+      })
+    );
+  });
 
-    return (data, state, action) => {
-        // @todo: #4.2 — обработать очистку поля
-
-        // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data;
+  return (data, state, action) => {
+    // @todo: #4.2 — обработать очистку поля
+    if (action && action.name === "clear") {
+      const parentElement = action.parentElement;
+      const input = parentElement.querySelector("input");
+      if (input) {
+        input.value = null;
+        state[action.dataset.field] = null;
+      }
     }
+
+    // @todo: #4.5 — отфильтровать данные используя компаратор
+    return data.filter((row) => compare(row, state));
+  };
 }
