08f6626dfdba9cb07c132c5c6c452722c15cc76c
rnkhasanov <RNKhasanov@kzn.beeline.ru>
2026-03-06 10:23:19 +0300

    change initData body to api query

--- src/data.js
+++ src/data.js
@@ -1,9 +1,14 @@
 import {makeIndex} from "./lib/utils.js";
 
+const BASE_URL = 'https://webinars.webdev.education-services.ru/sp7-api';
+
 export function initData(sourceData) {
-    const sellers = makeIndex(sourceData.sellers, 'id', v => `${v.first_name} ${v.last_name}`);
-    const customers = makeIndex(sourceData.customers, 'id', v => `${v.first_name} ${v.last_name}`);
-    const data = sourceData.purchase_records.map(item => ({
+    let sellers;
+    let customers;
+    let lastResult;
+    let lastQuery;
+
+    const mapRecords = (data) => data.map(item => ({
         id: item.receipt_id,
         date: item.date,
         seller: sellers[item.seller_id],
@@ -12,15 +17,36 @@ export function initData(sourceData) {
     }));
 
     const getIndexes = async () => {
+        if (!sellers || !customers) { // если индексы ещё не установлены, то делаем запросы
+            [sellers, customers] = await Promise.all([ // запрашиваем и деструктурируем в уже объявленные ранее переменные
+                fetch(`${BASE_URL}/sellers`).then(res => res.json()), // запрашиваем продавцов
+                fetch(`${BASE_URL}/customers`).then(res => res.json()), // запрашиваем покупателей
+            ]);
+        }
+
         return { sellers, customers };
     };
 
     const getRecords = async () => {
-        return {
-            total: data.length,
-            items: data
+        const qs = new URLSearchParams(query); // преобразуем объект параметров в SearchParams объект, представляющий query часть url
+        const nextQuery = qs.toString(); // и приводим к строковому виду
+
+        if (lastQuery === nextQuery && !isUpdated) { // isUpdated параметр нужен, чтобы иметь возможность делать запрос без кеша
+            return lastResult; // если параметры запроса не поменялись, то отдаём сохранённые ранее данные
+        }
+
+        // если прошлый квери не был ранее установлен или поменялись параметры, то запрашиваем данные с сервера
+        const response = await fetch(`${BASE_URL}/records?${nextQuery}`);
+        const records = await response.json();
+
+        lastQuery = nextQuery; // сохраняем для следующих запросов
+        lastResult = {
+            total: records.total,
+            items: mapRecords(records.items)
         };
-    }
+
+        return lastResult;
+    };
 
     return {
         getIndexes,
